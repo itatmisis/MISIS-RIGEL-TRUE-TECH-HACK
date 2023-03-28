c38412958df4726335dea47000e0d1a2d2b8d709
wtfnukee <hehenukee@gmail.com>
2023-03-28 15:03:33 +0300

    Update ml.py

--- ml/ml.py
+++ ml/ml.py
@@ -1,7 +1,4 @@
 from math import sqrt, cos, pi
-
-import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -17,55 +14,36 @@ def y(k, N):
 
 
 def spatial_smoothing(l):
-    print(l.shape)
-    print(l)
     for i, row in enumerate(l):
         if i == 0:
             l[i] = (l[i] + l[i + 1]) / 2
-        elif i == len(I)-1:
+        elif i == len(I) - 1:
             l[i] = (l[i - 1] + l[i]) / 2
         else:
             l[i] = (l[i - 1] + l[i] + l[i + 1]) / 3
     return l
 
 
-cap = cv2.VideoCapture('v.mp4')
-
-if not cap.isOpened():
-    print("Error opening video stream or file")
-i = 0
-I = []
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret:
-        cv2.imshow('Frame', frame)
-        if i % 30 == 0:
-            fig, ax = plt.subplots()
-            # plt.imshow(frame)
-            colors = ("red", "green", "blue")
-            # plt.figure()
-            plt.xlim([0, 256])
-            for channel_id, color in enumerate(colors):
-                histogram, bin_edges = np.histogram(
-                    frame[:, :, channel_id], bins=256, range=(0, 256)
-                )
-                # plt.plot(bin_edges[0:-1], histogram, color=color)
-
+def getI(video: np.ndarray, threshold: float) -> np.ndarray:
+    for i, frame in enumerate(video):
         if i != 0:
-            diff: np.array = frame - prev
+            diff: np.ndarray = frame - prev
             I.append(diff.sum(axis=1))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-        i += 1
-        prev = frame
-    else:
-        break
+    I = np.array(I)
+    I = spatial_smoothing(I)
+    I = I.sum(axis=0).mean(axis=1)
+    I -= np.mean(I)
+    I /= np.std(I)
+    I = I > threshold
+    return I
 
-cap.release()
 
-cv2.destroyAllWindows()
+def getDangerMapping(video, I):
+    return dict(zip(video, I))
 
-I = np.array(I)
-I = spatial_smoothing(I)
-threshold = 655352
-print(I.sum(axis=0).mean(axis=1) > threshold)
+
+def epilepsy_task(image, dangerMapping, threshold):
+    I = getI(video, threshold)
+    dangerMapping = getDangerMapping(video, I)
+    strength = dangerMapping[image]
+    return gaussianBlur(image, strength)
