1aedd34bdd646adc5b1222e40ae27d68a118cb2b
wtfnukee <hehenukee@gmail.com>
2023-03-28 15:10:39 +0300

    Refactoring

--- ml/ml.py
+++ ml/ml.py
@@ -1,6 +1,6 @@
-from math import sqrt, cos, pi
 import numpy as np
-
+from numpy import sqrt, cos, pi
+from typing import Iterable, Dict
 
 def w(k, N):
     if k == 1:
@@ -13,12 +13,10 @@ def y(k, N):
     return w(k, N) * sum(n * cos((pi / (2 * n)) * (2 * n - 1) * (k - 1)) for n in range(1, N + 1))
 
 
-def spatial_smoothing(l):
+def spatial_smoothing(l:Iterable) -> Iterable:
     for i, row in enumerate(l):
-        if i == 0:
+        if (i == 0) or (i == len(I) - 1):
             l[i] = (l[i] + l[i + 1]) / 2
-        elif i == len(I) - 1:
-            l[i] = (l[i - 1] + l[i]) / 2
         else:
             l[i] = (l[i - 1] + l[i] + l[i + 1]) / 3
     return l
@@ -38,11 +36,11 @@ def getI(video: np.ndarray, threshold: float) -> np.ndarray:
     return I
 
 
-def getDangerMapping(video, I):
+def getDangerMapping(video:np.ndarray, I:np.ndarray) -> Dict:
     return dict(zip(video, I))
 
 
-def epilepsy_task(image, dangerMapping, threshold):
+def epilepsy_task(image:np.ndarray, dangerMapping:Dict, threshold:float) -> np.ndarray:
     I = getI(video, threshold)
     dangerMapping = getDangerMapping(video, I)
     strength = dangerMapping[image]
